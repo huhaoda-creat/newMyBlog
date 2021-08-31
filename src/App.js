fdcc11ed860295af1db3ec430f73cb8e3b3b30c4
胡豪达 <hu20182021@163.com>
2021-08-31 17:04:34 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-
-import { Component } from 'react';
-import {Route,Switch} from 'react-router-dom'
-import Login from "./pages/login/login"
-import Admin from "./pages/admin/admin"
-class App extends Component {
-  render(){
-    return (
-      <div className='App'>
-        <Switch>
-          <Route path='/login' component={Login}  exact/>
-          <Route path='/admin' component={Admin} />
-        </Switch>
-      </div>
-  );
+import React,{Component} from "react";
+export default class App extends Component{
+    render(){
+        return(
+            <div>
+                App
+            </div>
+        )
+    }
 }
-}
-export default App;
